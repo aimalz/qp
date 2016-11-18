71272b0d8f3dfd829af5781bc16fa09f51b7a810
Phil Marshall <dr.phil.marshall@gmail.com>
2016-11-17 16:44:06 -0800

    Nicer denominator: 9 points now gives 0.1,0.2,0.3 etc

--- pdf.py
+++ pdf.py
@@ -14,14 +14,32 @@ class PDF(object):
 
         return
 
-    def quantize(self, num_points):#inverse CDF (PPF)
+    def quantize(self, num_points):
+        """
+        Computes an array of evenly-spaced quantiles.
 
-        quanta = 1./num_points
-        points = np.linspace(0.+quanta, 1.-quanta, num_points)
-        print(points)
+        Parameters
+        ----------
+        num_points : int
+            The number of quantiles to compute.
+
+        Returns
+        -------
+        self.quantiles : ndarray, float
+            The quantile points.
+
+        Comments
+        --------
+        Uses the `.ppf` method of the `rvs_continuous` distribution
+        object stored in `self.truth`. This calculates the inverse CDF.
+        See `the Scipy docs <https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rv_continuous.ppf.html#scipy.stats.rv_continuous.ppf>`_ for details.
+        """
+        # Find the spacing of the quantiles, and make an array of them:
+        quantum = 1.0/float(num_points+1)
+        points = np.linspace(0.0+quantum, 1.0-quantum, num_points)
+        print("Calculating quantiles: ", points)
         self.quantiles = self.truth.ppf(points)
-        print(self.quantiles)
-
+        print("Result: ", self.quantiles)
         return self.quantiles
 
     def interpolate(self):
