dd1c220606a1d7a40b9d3def1acbbfd03a865004
Phil Marshall <dr.phil.marshall@gmail.com>
2016-12-05 13:27:23 -0800

    Include links to source code with viewcode extension

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon' ]
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode']
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
