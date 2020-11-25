a563ae498404385311978a5c0ad5fdbd55d917ff
Eric Charles <echarles@gmail.com>
2020-11-25 09:30:21 -0800

    changed convert to extract in extraction function names

--- qp/conversion_funcs.py
+++ qp/conversion_funcs.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from sklearn import mixture
 
-def convert_using_vals_at_x(in_dist, **kwargs):
+def extract_vals_at_x(in_dist, **kwargs):
     """Convert using a set of x and y values.
 
     Parameters
@@ -27,12 +27,12 @@ def convert_using_vals_at_x(in_dist, **kwargs):
     """
     xvals = kwargs.pop('xvals', None)
     if xvals is None: # pragma: no cover
-        raise ValueError("To convert to convert_using_xy_vals you must specify xvals")
+        raise ValueError("To convert to extract_xy_vals you must specify xvals")
     yvals = in_dist.pdf(xvals)
     return dict(xvals=xvals, yvals=yvals)
 
 
-def convert_using_xy_vals(in_dist, **kwargs):
+def extract_xy_vals(in_dist, **kwargs):
     """Convert using a set of x and y values.
 
     Parameters
@@ -52,13 +52,13 @@ def convert_using_xy_vals(in_dist, **kwargs):
     """
     xvals = kwargs.pop('xvals', None)
     if xvals is None: # pragma: no cover
-        raise ValueError("To convert using convert_using_xy_vals you must specify xvals")
+        raise ValueError("To convert using extract_xy_vals you must specify xvals")
     yvals = in_dist.pdf(xvals)
     expand_x = np.ones(yvals.shape) * np.squeeze(xvals)
     return dict(xvals=expand_x, yvals=yvals)
 
 
-def convert_using_samples(in_dist, **kwargs):
+def extract_samples(in_dist, **kwargs):
     """Convert using a set of values sampled from the PDF
 
     Parameters
@@ -81,7 +81,7 @@ def convert_using_samples(in_dist, **kwargs):
     return dict(samples=samples, xvals=xvals, yvals=None)
 
 
-def convert_using_hist_values(in_dist, **kwargs):
+def extract_hist_values(in_dist, **kwargs):
     """Convert using a set of values sampled from the PDF
 
     Parameters
@@ -101,12 +101,12 @@ def convert_using_hist_values(in_dist, **kwargs):
     """
     bins = kwargs.pop('bins', None)
     if bins is None: # pragma: no cover
-        raise ValueError("To convert using convert_using_hist_samples you must specify bins")
+        raise ValueError("To convert using extract_hist_samples you must specify bins")
     bins, pdfs = in_dist.histogramize(bins)
     return dict(bins=bins, pdfs=pdfs)
 
 
-def convert_using_hist_samples(in_dist, **kwargs):
+def extract_hist_samples(in_dist, **kwargs):
     """Convert using a set of values samples that are then histogramed
 
     Parameters
@@ -129,7 +129,7 @@ def convert_using_hist_samples(in_dist, **kwargs):
     bins = kwargs.pop('bins', None)
     size = kwargs.pop('size', 1000)
     if bins is None: # pragma: no cover
-        raise ValueError("To convert using convert_using_hist_samples you must specify bins")
+        raise ValueError("To convert using extract_hist_samples you must specify bins")
     samples = in_dist.rvs(size=size)
 
     def hist_helper(sample):
@@ -139,7 +139,7 @@ def convert_using_hist_samples(in_dist, **kwargs):
     return dict(bins=bins, pdfs=pdfs)
 
 
-def convert_using_quantiles(in_dist, **kwargs):
+def extract_quantiles(in_dist, **kwargs):
     """Convert using a set of quantiles and the locations at which they are reached
 
     Parameters
@@ -159,12 +159,12 @@ def convert_using_quantiles(in_dist, **kwargs):
     """
     quants = kwargs.pop('quants', None)
     if quants is None: # pragma: no cover
-        raise ValueError("To convert using convert_using_quantiles you must specify quants")
+        raise ValueError("To convert using extract_quantiles you must specify quants")
     locs = in_dist.ppf(quants)
     return dict(quants=quants, locs=locs)
 
 
-def convert_using_fit(in_dist, **kwargs): # pragma: no cover
+def extract_fit(in_dist, **kwargs): # pragma: no cover
     """Convert to a functional distribution by fitting it to a set of x and y values
 
     Parameters
@@ -182,14 +182,14 @@ def convert_using_fit(in_dist, **kwargs): # pragma: no cover
     data : `dict`
         The extracted data
     """
-    raise NotImplementedError('convert_using_fit')
+    raise NotImplementedError('extract_fit')
     #xvals = kwargs.pop('xvals', None)
     #if xvals is None:
-    #   raise ValueError("To convert using convert_using_fit you must specify xvals")
+    #   raise ValueError("To convert using extract_fit you must specify xvals")
     ##vals = in_dist.pdf(xvals)
 
 
-def convert_using_mixmod_fit_samples(in_dist, **kwargs):
+def extract_mixmod_fit_samples(in_dist, **kwargs):
     """Convert to a mixture model using a set of values sample from the pdf
 
     Parameters
