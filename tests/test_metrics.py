ca9a09f91af1ccafc7f22946bb252efd7c7d28b5
Max West <maxwest@uw.edu>
2022-08-04 15:32:36 -0700

    bug fix in the calculate_kld function + better error messaging in metrics + expanded unit tests for metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -4,8 +4,10 @@ Unit tests for PDF class
 
 import unittest
 import qp
+import numpy as np
 
 from qp import test_funcs
+from qp.utils import epsilon
 
 
 class MetricTestCase(unittest.TestCase):
@@ -18,16 +20,46 @@ class MetricTestCase(unittest.TestCase):
         self.ens_n = test_funcs.build_ensemble(qp.stats.norm_gen.test_data['norm'])  #pylint: disable=no-member
         self.ens_n_shift = test_funcs.build_ensemble(qp.stats.norm_gen.test_data['norm_shifted'])  #pylint: disable=no-member
 
+        locs = 2* (np.random.uniform(size=(10,1))-0.5)
+        scales = 1 + 0.2*(np.random.uniform(size=(10,1))-0.5)
+        self.ens_n_plus_one = qp.Ensemble(qp.stats.norm, data=dict(loc=locs, scale=scales)) #pylint: disable=no-member
+
+        bins = np.linspace(-5, 5, 11)
+        self.ens_s = self.ens_n.convert_to(qp.spline_gen, xvals=bins, method="xy")
+
     def tearDown(self):
         """ Clean up any mock data files created by the tests. """
 
     def test_kld(self):
         """ Test the calculate_kld method """
-        _ = qp.metrics.calculate_kld(self.ens_n, self.ens_n_shift, limits=(0.,2.5))
+        kld = qp.metrics.calculate_kld(self.ens_n, self.ens_n_shift, limits=(0.,2.5))
+        assert np.all(kld == 0.)
+
+    def test_kld_different_npdf(self):
+        """ Ensure that the kld function fails when trying to compare ensembles of different sizes. """
+        with self.assertRaises(ValueError) as context:
+            qp.metrics.calculate_kld(self.ens_n, self.ens_n_plus_one, limits=(0.,2.5))
+
+        self.assertTrue('Cannot calculate KLD between two ensembles with different number of PDFs' in str(context.exception))
+
+    def test_broken_kld(self):
+        """ Test to see if the calculate_kld function responds appropriately to negative results """
+        kld = qp.metrics.calculate_kld(self.ens_n, self.ens_s, limits=(0.,2.5))
+
+        assert np.all(kld == epsilon)
+        
 
     def test_rmse(self):
         """ Test the calculate_rmse method """
-        _ = qp.metrics.calculate_rmse(self.ens_n, self.ens_n_shift, limits=(0.,2.5))
+        rmse = qp.metrics.calculate_rmse(self.ens_n, self.ens_n_shift, limits=(0.,2.5))
+        assert np.all(rmse == 0.)
+
+    def test_rmse_different_npdf(self):
+        """ Ensure that the rmse function fails when trying to compare ensembles of different sizes. """
+        with self.assertRaises(ValueError) as context:
+            qp.metrics.calculate_rmse(self.ens_n, self.ens_n_plus_one, limits=(0.,2.5))
+
+        self.assertTrue('Cannot calculate RMSE between two ensembles with different number of PDFs' in str(context.exception))
 
 
 if __name__ == '__main__':
